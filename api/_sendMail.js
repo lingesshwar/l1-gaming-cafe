ff8e04337f74cc872f83f5002cfd0e447fb15bb3
lingesshwar <lingesshwar@gmail.com>
2026-01-15 09:24:02 +0530

    Switch backend email to Resend (reliable)

--- api/_sendMail.js
+++ api/_sendMail.js
@@ -2,10 +2,14 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function sendMail({ to, subject, html }) {
-  await resend.emails.send({
+export default async function sendMail({ subject, html }) {
+  if (!process.env.RESEND_API_KEY) {
+    throw new Error("RESEND_API_KEY missing");
+  }
+
+  return await resend.emails.send({
     from: "L1 Gaming Cafe <onboarding@resend.dev>",
-    to,
+    to: [process.env.STAFF_EMAIL],
     subject,
     html,
   });
