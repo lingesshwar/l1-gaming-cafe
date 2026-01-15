c5eb7a065909651e9018337ed893c8f21112fa6d
lingesshwar <lingesshwar@gmail.com>
2026-01-15 09:36:08 +0530

    Fix Resend sender email to verified address

--- api/_sendMail.js
+++ api/_sendMail.js
@@ -3,13 +3,9 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function sendMail({ subject, html }) {
-  if (!process.env.RESEND_API_KEY) {
-    throw new Error("RESEND_API_KEY missing");
-  }
-
   return await resend.emails.send({
     from: "L1 Gaming Cafe <onboarding@resend.dev>",
-    to: [process.env.STAFF_EMAIL],
+    to: process.env.STAFF_EMAIL,
     subject,
     html,
   });
