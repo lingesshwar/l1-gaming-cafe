1fe2d0117660fbe6237b81ca68960e89cc59744a
lingesshwar <lingesshwar@gmail.com>
2026-01-15 10:01:36 +0530

    Fix email export + improve email UI

--- api/_sendMail.js
+++ api/_sendMail.js
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendBookingEmail(data) {
+export default async function sendMail(data) {
   return await resend.emails.send({
     from: "L1 Gaming Cafe <sec24sc020@sairamtap.edu.in>",
     to: process.env.STAFF_EMAIL,
